90bc4f61869f3748ddd2c316b12f73808b255d20
Jonathan Li <jonathanli.chocolate@gmail.com>
2018-02-01 02:05:05 -0500

    Big update: added processing for sequence IDs and strategy datapoints, as well as simple visualizations in holoviews
    Signed-off-by: Jonathan Li <jonathanli.chocolate@gmail.com>

--- infoset.py
+++ infoset.py
@@ -7,20 +7,22 @@ class InfoSet():
 
     def __init__(self, gameFileLine):
         self.line = gameFileLine
-        self.ID = self.getID(gameFileLine)
+        self.name = self.getName(gameFileLine)
         self.player = self.getPlayer()
         self.members = self.getMembers()
         self.depth = self.getDepth()
         self.parent = self.getParent()
         self.children = set()
         self.actions = set()
-        self.probs = dict()
+        self.seqIDs = dict()
+        self.probs = []
+
 
     ##### GET methods for retrieving info set data #####
 
-    def getID(self, gameFileLine):
+    def getName(self, gameFileLine):
         '''
-        returns the InfoSet's ID
+        returns the InfoSet's name
         '''
         regMatch = re.match('infoset (.+?) nodes (.*)', gameFileLine)
         return regMatch.groups()[0]
@@ -29,7 +31,7 @@ class InfoSet():
         '''
         returns the InfoSet's player (the one yet to make a move)
         '''
-        lastTurn = self.ID.split('/')[-1]
+        lastTurn = self.name.split('/')[-1]
         regMatch = re.match('(.+?):(.+)', lastTurn)
         if regMatch:
             lastPlayer = regMatch.groups()[0]
@@ -39,7 +41,7 @@ class InfoSet():
 
     def getMembers(self):
         '''
-        returns a set of node IDs for the members of this infoset
+        returns a set of node names for the members of this infoset
         '''
         result = set()
         regMatch = re.match('(.+)nodes (.+)', self.line)
@@ -53,43 +55,44 @@ class InfoSet():
 
     def getParent(self):
         '''
-        returns the ID of the infoset's parent node in the decision tree
+        returns the name of the infoset's parent node in the decision tree
         '''
 
-        isRoot = len(self.ID.split('/')) <= 2
+        isRoot = len(self.name.split('/')) <= 2
 
         if isRoot:
             return ''
         else:
-            return '/'.join(self.ID.split('/')[:-2])
+            return '/'.join(self.name.split('/')[:-2])
 
     def getDepth(self):
         '''
         returns the infoset's depth in the game tree
         NOTE - depth is not for the decision tree
         '''
-        return self.ID.count('/') - 1
+        return self.name.count('/') - 1
 
     ##### ADD methods for adding data to the infoset #####
 
-    def addParent(self, parentID):
-        self.parents.add(parentID)
+    def addParent(self, parentName):
+        self.parents.add(parentName)
 
-    def addChildren(self, childID):
-        self.children.add(childID)
+    def addChildren(self, childName):
+        self.children.add(childName)
 
     def addAction(self,s):
         self.actions.add(s)
+        self.probs[s] = []
 
     def setProbs(self, action, prob):
         if action not in self.actions:
             self.addAction(action)
-        self.probs[action] = prob
+        self.probs[action].append(prob)
 
     ##### MISC methods #####
 
     def __hash__(self):
-        return hash(self.ID)
+        return hash(self.name)
 
     def toDict(self):
         '''
@@ -98,7 +101,7 @@ class InfoSet():
         '''
         res = dict()
 
-        res['ID'] = self.ID
+        res['ID'] = self.name
         res['player'] = self.player
         res['members'] = ' '.join(list(self.members))
         res['parent'] = self.parent
--- parsing.py
+++ parsing.py
@@ -1,35 +1,36 @@
+import json
 import re
 import sys
 from infoset import InfoSet, P1, P2
 
 def getSearchParam(infoset, parent=False):
 
-    chancePos = infoset.ID.find('?')
-    isP1 = (chancePos == len(infoset.ID) - 1 or
-            infoset.ID[chancePos+1] == '/')
+    chancePos = infoset.name.find('?')
+    isP1 = (chancePos == len(infoset.name) - 1 or
+            infoset.name[chancePos+1] == '/')
     if isP1:
-        chanceCards = infoset.ID[chancePos-1:chancePos+1]
+        chanceCards = infoset.name[chancePos-1:chancePos+1]
     else:
-        chanceCards = infoset.ID[chancePos:chancePos+2]
-    IDFormat = infoset.ID.replace(chanceCards, '([JKQ\?]+?)')
+        chanceCards = infoset.name[chancePos:chancePos+2]
+    nameFormat = infoset.name.replace(chanceCards, '([JKQ\?]+?)')
     #  we also need to make sure that we have the "C" in front for the first chance node
-    if IDFormat[1] != 'C':
-        IDFormat = '/C:' + IDFormat[1:]
-    return 'node ' + IDFormat + ' player(.+)actions (.+)'
+    if nameFormat[1] != 'C':
+        nameFormat = '/C:' + nameFormat[1:]
+    return 'node ' + nameFormat + ' player(.+)actions (.+)'
     
 def addFamily(infoset, parentSets):
     '''
     add each infoset to its parent's set of children
     '''
 
-    isRoot = len(infoset.ID.split('/')) <= 2
+    isRoot = len(infoset.name.split('/')) <= 2
     
     if isRoot:
         return
     else:
         parent = parentSets.get(infoset.parent, None)
         if parent:
-            parent.addChildren(infoset.ID)
+            parent.addChildren(infoset.name)
 
 def addAllFamily(p1Sets, p2Sets):
     '''
@@ -59,7 +60,7 @@ def addActions(infoset, f):
     try:
         actions = matchObj.groups()[-1]  # obtain the last string + delete the extra '\n'
     except Exception as e:
-        raise Exception('Failed to add action for infoset: %s' % infoset.ID)
+        raise Exception('Failed to add action for infoset: %s' % infoset.name)
     for action in actions.split(' '):
         if action != '':
             infoset.actions.add(action)
@@ -74,12 +75,12 @@ def addAllActions(pInfoSets,f):
         addActions(infoset, f)
 
 
-def process(filename):
+def processInfosets(filename):
     '''
     read all the infosets from a game file
     '''
 
-    #  {p1/p2}Nodes are dictionaries mapping InfoSet IDs to InfoSets
+    #  {p1/p2}Nodes are dictionaries mapping InfoSet names to InfoSets
     p1Sets = dict() 
     p2Sets = dict()
     
@@ -91,9 +92,9 @@ def process(filename):
             line = line[:-1]  # to get rid of the trailing newline
             curIS = InfoSet(line)
             if curIS.player == P1:
-                p1Sets[curIS.ID] = curIS
+                p1Sets[curIS.name] = curIS
             else:
-                p2Sets[curIS.ID] = curIS
+                p2Sets[curIS.name] = curIS
 
     #  Now the nodes are missing a couple of attributes: parents, children, actions, and probs
     addAllFamily(p1Sets, p2Sets)
@@ -103,7 +104,104 @@ def process(filename):
     f.close()
 
     return p1Sets, p2Sets
-            
+
+
+def processSeqIDs(p1Sets, p2Sets, seqFilename):
+    '''
+    read sequence IDs from seqFilename into p1/p2Sets
+    returns two dictionaries mapping sequence IDs to infoset names and actions
+    '''
+
+    f = open(seqFilename)
+    jObj = json.load(f)
+    d = json.JSONDecoder().decode(json.dumps(jObj))
+    f.close()
+
+    p1Seqs = d['player1']
+    p2Seqs = d['player2']
+
+    for seqDict in p1Seqs:
+        if seqDict['is_empty_sequence']:
+            continue
+        seqID = seqDict['sequence_id']
+        seqName = seqDict['infoset_name']
+        actionName = seqDict['action_name']
+        p1Sets[seqName].seqIDs[seqID] = actionName
+
+    for seqDict in p2Seqs:
+        if seqDict['is_empty_sequence']:
+            continue
+        seqID = seqDict['sequence_id']
+        seqName = seqDict['infoset_name']
+        actionName = seqDict['action_name']
+        p2Sets[seqName].seqIDs[seqID] = actionName
+
+
+def alreadyAddedIt(itDict, isDicts):
+    '''
+    returns true if the particular iterate in question has already
+    been added to the infosets in isDicts
+    '''
+
+    itNumber = itDict['iteration']
+    randIS = isDicts.values()[0]
+    return len(randIS.probs)*10 >= itNumber
+
+def normalize(isDicts):
+    '''
+    Normalize the probabilities for the last 
+    probability distributions added to each infoset
+    '''
+
+    for infoset in isDicts.values():
+        lastIt = infoset.probs[-1]
+        total = sum(lastIt.values())
+        normalized = dict()
+        for action, prob in lastIt.items():
+            normalized[action] = prob/total
+        infoset.probs[-1] = normalized
+    
+
+def addIterate(itDict, isDicts):
+    '''
+    adds the strategy profiles for the iterate represented by itDict
+    to the infosets in isDicts
+    '''
+
+    strategies = itDict['strategy']
+
+    for infoset in isDicts.values():
+        curIt = dict()
+        for seqID, action in infoset.seqIDs.items():
+            curIt[action] = strategies[seqID]
+        infoset.probs.append(curIt)
+
+    normalize(isDicts)
+    
+
+def getData(p1Sets, p2Sets, dataFilename,its=None):
+    '''
+    extracts iterate data from dataFilename for the particular player,
+    adding it to the infosets in infosetDict
+        - if its is provided as an argument, only adds up to its number
+          of iterates for each player
+    '''
+
+    f = open(dataFilename)
+    jObj = json.load(f)
+    itList = json.JSONDecoder().decode(json.dumps(jObj))
+    f.close()
+
+    lim = (len(itList) if (its == None or its < 0 or 2*its > len(itList))
+           else 2*its)
+
+    for i in range(lim):
+        curDict = itList[i]
+        curSets = p1Sets if curDict['player_id'] == 1 else p2Sets
+        if alreadyAddedIt(curDict,curSets):
+            continue
+        addIterate(curDict,curSets)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -116,4 +214,3 @@ if __name__ == '__main__':
     else:
         print('Provide a game file as an argument! ')
 
-        
