b1543600a84102e1dd431bdcc55ab0a3560a81c9
Jonathan Li <jonathanli.chocolate@gmail.com>
2018-05-23 23:07:44 -0400

    updated to show reach
    Signed-off-by: Jonathan Li <jonathanli.chocolate@gmail.com>

--- comp_algs.py
+++ comp_algs.py
@@ -7,7 +7,7 @@ import sys
 from holoviews.util import Dynamic
 from bokeh.application import Application
 from bokeh.application.handlers import FunctionHandler
-from bokeh.layouts import layout, widgetbox
+from bokeh.layouts import layout, widgetbox, column
 from bokeh.models import Select
 from bokeh.plotting import curdoc
 from parsing import parsing
@@ -24,6 +24,7 @@ SORTS = ['diff','square']
 SORT = sys.argv[3] if ((len(sys.argv) > 3 and sys.argv[3] in SORTS) or
                        sys.argv[3].startswith('reachIterate=')) else 'diff'
 ALGS = sys.argv[4:]
+ITERATE = int(SORT[len('reachIterate='):]) if SORT.startswith('reachIterate=') else 500
 
 renderer = hv.renderer('bokeh')
 
@@ -40,6 +41,7 @@ def render(obj):
 
 playerData = dict()  # map of algName -> (p1,p2)
 genFunctions = dict()  # map of algName -> genCurvesAlg
+tableFunctions = dict() # map of algName -> genTableAlg
 
 def makeGenerator(p1,p2,actions,alg):
     def genCurves(infoset=p1.keys()[0],player=PLAYER,**kwargs):
@@ -64,6 +66,15 @@ def makeGenerator(p1,p2,actions,alg):
 
     return genCurves
         
+def tableGenerator(p1,p2):
+    def genTable(infoset=p1.keys()[0],player=PLAYER,**kwargs):
+        '''
+        generate a Table object containing the reach of the infoset
+        '''
+        iS = p1[infoset] if player == 'P1' else p2[infoset]
+        label = 'Reach at iterate %d for %s' % (ITERATE, iS.alg)
+        return hv.Table(([iS.reach[ITERATE]],),[label])
+    return genTable
 
 for curAlg in ALGS:
 
@@ -75,13 +86,16 @@ for curAlg in ALGS:
 
     playerData[curAlg] = (p1,p2)
     genFunctions[curAlg] = makeGenerator(p1,p2,actions,curAlg)
+    tableFunctions[curAlg] = tableGenerator(p1,p2)
 
 def convDmap(iList, alg):
     istream = hv.streams.Stream.define('%s Infoset' % PLAYER,
                                        infoset=iList[0].name)()
     f = genFunctions[alg]
+    g = tableFunctions[alg]
     dmap = hv.DynamicMap(f,streams=[istream])
-    return iList, istream, dmap, alg    
+    table = hv.DynamicMap(g,streams=[istream])
+    return iList, istream, dmap, table, alg    
 
 depths = dict()  # map of algName -> 2D list of infosets at proper depths
 dmaps = []
@@ -121,9 +135,11 @@ def modify_doc(doc):
         n = len(mapList)
         iStreams = []
         hvplots = []
-        (arbList,arbStream,arbmap,arbAlg) = mapList[0]
-        for (l,s,d,a) in mapList:
+        tables = []
+        (arbList,arbStream,arbmap,arbTable,arbAlg) = mapList[0]
+        for (l,s,d,t,a) in mapList:
             hvplots += [renderer.get_plot(d, doc)]
+            tables += [renderer.get_plot(t,doc)]
             iStreams += [s]
         def update(attname, old, new):
             for stream in iStreams:
@@ -134,13 +150,13 @@ def modify_doc(doc):
                         options=[i.name for i in arbList],
                         width=200,height=100)
         select.on_change('value',update)
-        return select, hvplots
+        return select, hvplots, tables
 
     rows = [getPlots(mapList) for mapList in dmaps]
 
     # Combine the holoviews plot and widgets in a layout
 
-    matrix = [([widgetbox([s])] + [p.state for p in plots]) for (s,plots) in rows]
+    matrix = [([widgetbox([s])] + [column([p.state,t.state]) for p,t in zip(plots,tables)]) for (s,plots,tables) in rows]
     
     plot = layout(matrix,sizing_mode='fixed')
     
